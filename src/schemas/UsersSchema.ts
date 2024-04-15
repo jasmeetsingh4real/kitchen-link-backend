2d60ed0dddeb15fc50bc14084b9f61e459788680
Jasmeet Singh <jasmeetsingh4real1984@gmail.com>
2024-04-16 01:32:35 +0530

    bug fix

--- src/schemas/UsersSchema.ts
+++ src/schemas/UsersSchema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
+import { EnumUserRole } from "../types/AuthTypes";
 
 export const createUserSchema = z.object({
   email: z.string(),
   password: z.string(),
   fullName: z.string(),
+  role: z.nativeEnum(EnumUserRole).optional(),
 });
 export const loginUserSchema = z.object({
   email: z.string(),
