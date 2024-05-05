77207c4abfa8ed4e367d2aee2d69a2687586647b
Jasmeet Singh <jasmeetsingh4real1984@gmail.com>
2024-05-05 23:45:42 +0530

    bug fix in res schema

--- src/schemas/RestaurantSchemas.ts
+++ src/schemas/RestaurantSchemas.ts
@@ -2,16 +2,16 @@ import { z } from "zod";
 import { EnumRestaurantStatus } from "../types/RestaurentsTypes";
 
 export const restaurantSchema = z.object({
-  ownerId: z.string(),
-  restaurantName: z.string(),
-  openingTime: z.string(),
-  closingTime: z.string(),
+  ownerId: z.string().min(1),
+  restaurantName: z.string().min(1),
+  openingTime: z.date(),
+  closingTime: z.date(),
   restaurantEmail: z.string().email(),
-  restaurantContact: z.string(),
+  restaurantContact: z.string().min(1),
   stateId: z.number(),
   countryId: z.number(),
   cityId: z.number(),
-  streetAddress: z.string(),
+  streetAddress: z.string().min(1),
   status: z.nativeEnum(EnumRestaurantStatus).optional(),
 });
 
